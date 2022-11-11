7138910f6b5736e79e5a1adb261886df8bbdf8af
Kristina Lavrenyuk <115408741+KristinaLavrenyuk@users.noreply.github.com>
2022-11-11 18:49:02 +0300

    Add files via upload

---
+++ TaskDivisibility.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t, a, b, c, d;
+    cin >> t;
+
+    for (int i = 0; i < t; i++)
+    {
+        c = 0;
+        cin >> a >> b;
+        if (a % b != 0)
+        {
+            c = a / b;
+            d = b * (c + 1);
+            c = d - a;
+        }
+        cout << c << endl;
+    }
+    system("pause");
+}
